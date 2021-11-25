2321400f61556060d6f9e1b3d2def208fd7ba65f
llollcat <llollcat@yandex.ru>
2021-11-25 15:55:43 +0300

    added login and registration features

--- src/main/java/messengerClient/User.java
+++ src/main/java/messengerClient/User.java
@@ -1,19 +1,70 @@
 package messengerClient;
 
 import java.util.List;
+
 public class User {
-    public String login;
-    public String passwd;
-    public String userToken;
-    public String avatar;
-    public List<Integer> chats;
+
+    private static User instance = null;
 
 
-    public User(String login, String passwd, String userToken, String avatar, List<Integer> chats) {
+    private User() {
+
+    }
+
+    public static synchronized User getInstance() {
+        if (instance == null)
+            instance = new User();
+        return instance;
+    }
+
+
+    private String login = null;
+    private String passwd = null;
+    private String token = null;
+    private String avatar = null;
+    private List<Integer> chats = null;
+
+    public static void setInstance(User instance) {
+        User.instance = instance;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
         this.login = login;
+    }
+
+    public String getPasswd() {
+        return passwd;
+    }
+
+    public void setPasswd(String passwd) {
         this.passwd = passwd;
-        this.userToken = userToken;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
         this.avatar = avatar;
+    }
+
+    public List<Integer> getChats() {
+        return chats;
+    }
+
+    public void setChats(List<Integer> chats) {
         this.chats = chats;
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,13 @@ module Messenger.Client {
     requires javafx.graphics;
     requires javafx.fxml;
 
+
+    requires org.apache.httpcomponents.httpclient;
+    requires org.apache.commons.codec;
+    requires org.apache.httpcomponents.httpcore;
+    requires json.simple;
+
+
     opens messengerClient;
     opens messengerClient.controllers;
     requires java.desktop;
